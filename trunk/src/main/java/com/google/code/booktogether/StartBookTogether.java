d241df2ddf8594a97055ed36b68f47d0023128e8
kwon37xi <kwon37xi@b710224e-ef76-11dd-a979-a7afd4e285e1>
2009-01-31 10:01:20 +0000

    괜히 고쳐봄

--- trunk/src/main/java/com/google/code/booktogether/StartBookTogether.java
+++ trunk/src/main/java/com/google/code/booktogether/StartBookTogether.java
@@ -6,7 +6,7 @@ public class StartBookTogether {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("책 함께보기를 시작합니다.");
+		System.out.println("책 함께보기를 시작합니다.!");
 	}
 
 }
