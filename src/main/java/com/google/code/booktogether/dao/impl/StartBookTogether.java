c32d51064fbcca485562861fe3ba8b06b2e7ca6e
microflower <microflower@b710224e-ef76-11dd-a979-a7afd4e285e1>
2009-02-02 03:19:35 +0000

    패키지명 변경

---
+++ src/main/java/com/google/code/booktogether/dao/impl/StartBookTogether.java
@@ -0,0 +1,12 @@
+package com.google.code.booktogether.dao.impl;
+
+public class StartBookTogether {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("책 함께보기를 시작합니다.!!");
+	}
+
+}
