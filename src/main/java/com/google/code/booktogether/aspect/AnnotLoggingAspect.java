bbd3721b697a2e4ca652df2d900439ce8c8ece33
microflower <microflower@b710224e-ef76-11dd-a979-a7afd4e285e1>
2009-02-03 16:43:33 +0000

    transaction 추가 logging aop 추가 pom.xml 수정으로 aspectj 추가

---
+++ src/main/java/com/google/code/booktogether/aspect/AnnotLoggingAspect.java
@@ -0,0 +1,45 @@
+package com.google.code.booktogether.aspect;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect 
+public class AnnotLoggingAspect {
+	
+	private Logger log = Logger.getLogger(this.getClass());
+
+	@Before("execution(public * com.google.code.booktogether.service.*.*(..))")
+	public String beforeLogging(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName();
+		if (log.isInfoEnabled())
+			log.info("calling: " + methodName);
+		return methodName;
+	}
+
+	@AfterReturning(pointcut = "execution(public * com.google.code.booktogether.service.*.*(..))", returning = "ret")
+	public void returningLogging(JoinPoint joinPoint, Object ret) {
+		String methodName = joinPoint.getSignature().getName();
+		if (log.isInfoEnabled())
+			log.info("called successfully: " + methodName + " returns " + ret);
+	}
+
+	@AfterThrowing(pointcut = "execution(public * com.google.code.booktogether.service.*.*(..))", throwing = "ex")
+	public void throwingLogging(JoinPoint joinPoint, Throwable ex) {
+		String methodName = joinPoint.getSignature().getName();
+		if (log.isInfoEnabled())
+			log.info("exception occured: " + methodName + " throws "
+					+ ex.getClass().getName());
+	}
+
+	@After("execution(public * com.google.code.booktogether.service.*.*(..))")
+	public void afterLogging(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName();
+		if (log.isInfoEnabled())
+			log.info("finish call: " + methodName);
+	}
+}
