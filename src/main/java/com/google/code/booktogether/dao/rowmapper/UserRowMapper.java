1c0abbf3ffc582650c4b60497f3a95966124de00
microflower <microflower@b710224e-ef76-11dd-a979-a7afd4e285e1>
2009-02-16 11:45:04 +0000

    SQL 변경

--- src/main/java/com/google/code/booktogether/dao/rowmapper/UserRowMapper.java
+++ src/main/java/com/google/code/booktogether/dao/rowmapper/UserRowMapper.java
@@ -27,15 +27,15 @@ public class UserRowMapper implements ParameterizedRowMapper<User>,Serializable{
 		User user=new User();
 		user.setUserAddInfo(new UserAddInfo());
 		
-		user.setIdNum(rs.getInt("ID"));
+		user.setIdNum(rs.getInt("IDNUM"));
 		user.setUserId(rs.getString("USER_ID"));
 		user.setEmail(rs.getString("EMAIL"));
 		user.setNickname(rs.getString("NICKNAME"));
 		user.setName(rs.getString("NAME"));
-		user.setIsDelete(rs.getInt("DELETE_Y_N"));
+		user.setIsDelete(rs.getInt("ISDELETE"));
 		user.setInputDate(rs.getDate("INPUT_DATE"));
 		
-		user.getUserAddInfo().setIdNum(rs.getInt("UAI_ID"));
+		user.getUserAddInfo().setIdNum(rs.getInt("UAI_IDNUM"));
 		user.getUserAddInfo().setBlog(rs.getString("BLOG"));
 		user.getUserAddInfo().setThumnail(rs.getString("THUMNAIL"));
 		
