4fa84a49bae9834740ec95c958d0332d3f822a4c
microflower <microflower@b710224e-ef76-11dd-a979-a7afd4e285e1>
2009-02-17 08:21:40 +0000

    Interceptor 추가   - 로그인 체크 기능 추가   - 해당 URL만 체크하기 기능(applicationContext.xml) 수정

---
+++ trunk/src/main/java/com/google/code/booktogether/web/interceptor/handler/SelectedAnnotationHandlerMapping.java
@@ -0,0 +1,43 @@
+package com.google.code.booktogether.web.interceptor.handler;
+
+import java.util.ArrayList;
+import org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping;
+
+public class SelectedAnnotationHandlerMapping extends
+		DefaultAnnotationHandlerMapping {
+
+	private ArrayList<String> urlList;
+
+	public SelectedAnnotationHandlerMapping() {
+
+	}
+
+	public void setUrls(ArrayList<String> arrayList) {
+		urlList = arrayList;
+	}
+
+	public String[] getFiltered(String as[]) {
+
+		if (as == null) {
+			return null;
+		}
+
+		ArrayList<String> arraylist = new ArrayList<String>();
+
+		for (String s : as) {
+
+			if (urlList.contains(s)) {
+				arraylist.add(s);
+			}
+
+		}
+
+		return (String[]) arraylist.toArray(new String[arraylist.size()]);
+	}
+
+	@Override
+	protected String[] determineUrlsForHandler(String s) {
+		return getFiltered(super.determineUrlsForHandler(s));
+	}
+
+}
