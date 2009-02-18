8892ed6d53f056d85ee625db7b3cff1e7edd839f
microflower <microflower@b710224e-ef76-11dd-a979-a7afd4e285e1>
2009-02-18 15:32:46 +0000

    테이블정보 수정

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,152 +1,169 @@
-/**
- * 1.지은이
- */
+
+/*1.지은이 */
 
 CREATE TABLE `author` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `name` varchar(20) NOT NULL COMMENT '지은이이름',
-  `author_div` int(1) NOT NULL COMMENT '지은이구분',
-  `book_idNum` int(11) NOT NULL COMMENT '책일련번호',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `author_div` int(1) NOT NULL,
+  `book_idNum` int(11) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 2.책
- */
+/*2.책 */
+
 CREATE TABLE `book` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `name` varchar(100) NOT NULL COMMENT '책이름',
-  `isbn10` char(10) NOT NULL COMMENT 'ISBN10자리',
-  `isbn13` char(13) NOT NULL COMMENT 'ISBN13자리',
-  `publish_comp` varchar(20) NOT NULL COMMENT '출판사',
-  `publish_date` varchar(10) NOT NULL COMMENT '출판일',
-  `price` int(5) DEFAULT NULL COMMENT '가격',
-  `corver` varchar(100) DEFAULT NULL COMMENT '책표지이미지',
-  `category` varchar(50) DEFAULT NULL COMMENT '카테고리',
-  `description` text COMMENT '설명',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `isbn10` char(10) NOT NULL,
+  `isbn13` char(13) NOT NULL,
+  `publish_comp` varchar(20) NOT NULL,
+  `publish_date` varchar(10) NOT NULL,
+  `price` int(5) DEFAULT NULL,
+  `corver` varchar(100) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `description` text,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-/**
- * 3. 책 별점
- */
+/*3.책 별점 */
 
 CREATE TABLE `bookgrade` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `book_idNum` int(11) NOT NULL COMMENT '책',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자',
-  `grade` int(1) NOT NULL COMMENT '별점',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `book_idNum` int(11) NOT NULL,
+  `user_idNum` int(11) NOT NULL,
+  `grade` int(1) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 4. 책 인용구
- */
+/*4. 인용구 */
+
 CREATE TABLE `bookmark` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자 일련번호',
-  `book_idNum` int(11) NOT NULL COMMENT '책 일련번호',
-  `vibe_num` int(11) NOT NULL COMMENT '공감수',
-  `page` int(11) DEFAULT NULL COMMENT '페이지',
-  `input_date` datetime NOT NULL COMMENT '등록날짜',
-  `content` varchar(200) NOT NULL COMMENT '인용구',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `book_idNum` int(11) NOT NULL,
+  `vibe_num` int(11) NOT NULL,
+  `page` int(11) DEFAULT NULL,
+  `input_date` datetime NOT NULL,
+  `content` varchar(200) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 5. 책 리뷰
- */
+/*5. 리뷰  */
 
 CREATE TABLE `bookreview` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `book_idNum` int(11) NOT NULL COMMENT '책',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자',
-  `recommend` int(11) DEFAULT NULL COMMENT '추천수',
-  `title` varchar(50) NOT NULL COMMENT '제목',
-  `review` text COMMENT '내용',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `book_idNum` int(11) NOT NULL,
+  `user_idNum` int(11) NOT NULL,
+  `recommend` int(11) DEFAULT NULL,
+  `title` varchar(50) NOT NULL,
+  `review` text,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 6. 책 리뷰 추천
- */
+/*6. 내서재 */
+
+CREATE TABLE `library` (
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `isopen` int(1) NOT NULL,
+  `notice` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*7. 내서재 */
+
+CREATE TABLE `librarybook` (
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `book_idNum` int(11) NOT NULL,
+  `library_idNum` int(11) NOT NULL,
+  `read_Date` datetime DEFAULT NULL,
+  `state` int(1) DEFAULT NULL,
+  `ispossess` int(1) DEFAULT NULL,
+  PRIMARY KEY (`idNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*8. 보유하고 있는 책 */
+
+CREATE TABLE `possessbook` (
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `book_idNum` int(11) NOT NULL,
+  `purchase_date` datetime DEFAULT NULL,
+  `purchase_price` datetime DEFAULT NULL,
+  `begin_read` datetime DEFAULT NULL,
+  `end_read` datetime DEFAULT NULL,
+  `quality` int(1) DEFAULT NULL,
+  `state` int(1) DEFAULT NULL,
+  PRIMARY KEY (`idNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*9. 리뷰 추천 */
 
 CREATE TABLE `recommend` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자일련번호',
-  `review_idNum` int(11) NOT NULL COMMENT '리뷰일련번호',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `review_idNum` int(11) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-/**
- * 7. 사용자 
- */
+/*10.사용자 */
 
 CREATE TABLE `user` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_id` varchar(15) NOT NULL COMMENT '사용자ID',
-  `email` varchar(30) NOT NULL COMMENT '이메일',
-  `nickname` varchar(20) NOT NULL COMMENT '별명',
-  `name` varchar(20) NOT NULL COMMENT '이름',
-  `isdelete` int(1) NOT NULL COMMENT '탈퇴유무',
-  `input_date` datetime NOT NULL COMMENT '가입날짜',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(15) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `isdelete` int(1) NOT NULL,
+  `input_date` datetime NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 8. 사용자 비밀번호
- */
+/*11.사용자 비밀번호 */
+
 CREATE TABLE `user_pw` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자 일련번호 ',
-  `pw` varbinary(50) NOT NULL COMMENT '비밀번호 Digest한것',
-  `salt` varbinary(50) NOT NULL COMMENT '비밀번호 Key역활',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `pw` varbinary(50) NOT NULL,
+  `salt` varbinary(50) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 9. 사용자 추가정보
- */
+/*12.사용자 추가정보*/
 
 CREATE TABLE `useraddinfo` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자일련번호',
-  `blog` varchar(50) DEFAULT NULL COMMENT '블로그주소 ',
-  `thumnail` varchar(50) DEFAULT NULL COMMENT '썸네일이미지파일명',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `blog` varchar(50) DEFAULT NULL,
+  `thumnail` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 10. 인용구 공감하기
- */
+/*13.인용구 북마크 */
+
 CREATE TABLE `vibe` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자일련번호',
-  `bookmark_idNum` int(11) NOT NULL COMMENT '북마크일련번호',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `bookmark_idNum` int(11) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 11. 생활반경 우편번호
- */
+/*14. 우편번호 */
+
 CREATE TABLE `zipcode` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `zipcode` varchar(7) NOT NULL COMMENT '우편번호',
-  `sido` varchar(4) DEFAULT NULL COMMENT '특별시,광역시,도',
-  `gugun` varchar(15) DEFAULT NULL COMMENT '시,군,구',
-  `dong` varchar(52) DEFAULT NULL COMMENT '동,읍,면',
+  `idNum` int(11) NOT NULL,
+  `zipcode` varchar(7) NOT NULL,
+  `sido` varchar(4) DEFAULT NULL,
+  `gugun` varchar(15) DEFAULT NULL,
+  `dong` varchar(52) DEFAULT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/**
- * 12. 생활반경
- */
+/*15.생활반경 */
+
 CREATE TABLE `zone` (
-  `idNum` int(11) NOT NULL AUTO_INCREMENT COMMENT '일련번호',
-  `user_idNum` int(11) NOT NULL COMMENT '사용자일련번호',
-  `zone` int(11) NOT NULL COMMENT '주소일련번호',
+  `idNum` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idNum` int(11) NOT NULL,
+  `zone` int(11) NOT NULL,
   PRIMARY KEY (`idNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
